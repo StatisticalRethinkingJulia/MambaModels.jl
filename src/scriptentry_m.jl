0051427d089e28741c4890b0106d061711cf2391
Rob J Goedman <goedman@mac.com>
2019-02-05 09:38:09 -0700

    Renamed chapter 2 model

--- src/scriptentry_m.jl
+++ src/scriptentry_m.jl
@@ -1,6 +1,6 @@
 script_dict_m = DataStructures.OrderedDict{AbstractString, Vector{ScriptEntry}}(
   "02" => [
-    scriptentry("clip-08m.jl")
+    scriptentry("m2.1m.jl")
   ],
   "04" => [
     scriptentry("m4.4m.jl")
