b44c078fa75866ebb2587b257b31e4d548413ac4
Rob J Goedman <goedman@mac.com>
2019-02-08 19:00:08 -0700

    Updated script_dict_m

--- src/scriptentry_m.jl
+++ src/scriptentry_m.jl
@@ -1,6 +1,6 @@
 script_dict_m = DataStructures.OrderedDict{AbstractString, Vector{ScriptEntry}}(
   "02" => [
-    scriptentry("m2.1m.jl", exe=false, doc=false)
+    scriptentry("m2.1m.jl")
   ],
   "04" => [
     scriptentry("m4.4m.jl", exe=false, doc=false)
