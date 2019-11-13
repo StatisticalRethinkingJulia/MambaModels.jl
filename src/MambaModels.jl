ffb3038c949883d6d009300cf07647f9c4713098
Rob J Goedman <goedman@mac.com>
2019-11-12 21:34:49 -0700

    Initial updates for v1.0

--- src/MambaModels.jl
+++ src/MambaModels.jl
@@ -2,34 +2,8 @@ module MambaModels
 
 using Reexport 
 
-@reexport using StatisticalRethinking, Distributions, RDatasets, DataFrames
-@reexport using StatsBase, StatsPlots, StatsFuns, Plots 
-@reexport using CSV, DelimitedFiles, Serialization
-@reexport using MCMCChains, Mamba
-
-using DataStructures
-
-const src_path_m = @__DIR__
-
-"""
-
-# rel_path_m
-
-Relative path using the MambaModels src/ directory.
-
-### Example to get access to the data subdirectory
-```julia
-rel_path_m("..", "data")
-```
-"""
-rel_path_m(parts...) = normpath(joinpath(src_path_m, parts...))
-
-include("scriptentry_m.jl")
-include("generate_m.jl")
-
-export
-  rel_path_m,
-  script_dict_m,
-  generate_m
+@reexport using Mamba
+@reexport using CSV, DataFrames, Distributions
+@reexport using MCMCChains, StatsFuns, StatsPlots
 
 end # module
--- src/scriptentry_m.jl
+++
@@ -1,8 +0,0 @@
-script_dict_m = DataStructures.OrderedDict{AbstractString, Vector{ScriptEntry}}(
-  "02" => [
-    scriptentry("m2.1m.jl")
-  ],
-  "04" => [
-    scriptentry("m4.4m.jl")
-  ]
-);
